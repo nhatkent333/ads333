685ef0ebd676e4591a9cf4123a6ce3e2dd522641
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2022-07-01 13:27:20 +0700

    Update ads333.js

--- ads333.js
+++ ads333.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementsByClassName("ads333")[0].innerHTML = "<p><strong>CLIP HOT ĐƯỢC NHIỀU NGƯỜI QUAN TÂM</strong></p><div style="text-align:center; width:100%; height:auto;"><a href='https://www.youtube.com/watch?v=XTuBuKzF-_w'><img alt='my image' src='https://ads333.vieclamvui.com/pham-nhat-vuong-youtube.jpg'></a><div>";
+	document.getElementsByClassName("ads333")[0].innerHTML = "<p><strong>CLIP HOT ĐƯỢC NHIỀU NGƯỜI QUAN TÂM</strong></p><div style='text-align:center; width:100%; height:auto;'><a href='https://www.youtube.com/watch?v=XTuBuKzF-_w'><img alt='my image' src='https://ads333.vieclamvui.com/pham-nhat-vuong-youtube.jpg'></a><div>";
 }, false);
