dbaafe30788d321e26de6d623f23b1c7aa67e9f2
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2022-07-01 08:28:54 +0700

    Add files via upload

---
+++ ads333.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementsByClassName("ads333")[0].innerHTML = "<a href='https://www.youtube.com/watch?v=XTuBuKzF-_w'><img alt='my image' src='https://ads333.vieclamvui.com/pham-nhat-vuong-youtube.jpg'></a>";
+}, false);
