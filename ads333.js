ff5e8c430d1a98ef091bc6d915138f3a1fc03cb1
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2022-11-20 16:24:11 +0700

    Update ads333.js

--- ads333.js
+++ ads333.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementsByClassName("ads333")[0].innerHTML = "<div style='text-align:center; width:100%; height:auto;'><p><strong>CLIP HOT ĐƯỢC NHIỀU NGƯỜI QUAN TÂM</strong></p><a href='https://www.youtube.com/watch?v=XTuBuKzF-_w'><img alt='my image' src='https://ads333.vieclamvui.com/pham-nhat-vuong-youtube.jpg'></a><div>";
+	document.getElementsByClassName("ads333")[0].innerHTML = "<div style='text-align:center; width:100%; height:auto;'><p><strong>CLIP HOT ĐƯỢC NHIỀU NGƯỜI QUAN TÂM</strong></p><a href='https://youtu.be/XTuBuKzF-_w?t=588' target='_blank'><img alt='my image' src='https://ads333.vieclamvui.com/pham-nhat-vuong-youtube.jpg'></a><div>";
 }, false);
 
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementsByClassName("ads333right")[0].innerHTML = "<div style='position: fixed; bottom: 0; right: 0; z-index: 999; min-width: 0px; max-width: 600px; min-width: calc(0px); width: calc(59900% - 358801px); display: inline-block;'><a href='https://youtu.be/XTuBuKzF-_w?t=588' target='_blank' rel='noopener'><img src='https://ads333.vieclamvui.com/pham-nhat-vuong-youtube.jpg' alt='Balloon Right' width='100%' height='auto' loading='lazy'></a></div>";
+	document.getElementsByClassName("ads333right")[0].innerHTML = "<div style='position: fixed; bottom: 0; right: 0; z-index: 999; min-width: 0px; max-width: 600px; min-width: calc(0px); width: calc(59900% - 358801px); display: inline-block;'><a href='https://youtu.be/XTuBuKzF-_w?t=588' target='_blank' rel='noopener'><img src='https://ads333.vieclamvui.com/pham-nhat-vuong-youtube.jpg' width='100%' height='auto'></a></div>";
 }, false);
