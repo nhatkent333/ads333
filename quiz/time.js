0387283ea0074044415c82d5b3236f183f05184c
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2023-03-18 14:01:33 +0700

    Create time.js

---
+++ quiz/time.js
@@ -0,0 +1,5 @@
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("timeh1").innerHTML = year;
+document.getElementById("timecontent").innerHTML = year;
+document.getElementById("timehfooter").innerHTML = year;
