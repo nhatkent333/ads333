b25df3126e79438555bc20d45a4baa1f5b5bb6e9
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2023-11-21 14:00:43 +0700

    Update diepdownload.js

--- diepdownload.js
+++ diepdownload.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Select the link element
-    var link = document.querySelector('diep0000001 a');
+    var link = document.querySelector('.diep0000001 a');
 
     // Change the href attribute
     link.href = "https://docs.google.com/document/d/1eVqFosEcWvUr5p1mwwkw_s7U2BMgNzP2/edit";
