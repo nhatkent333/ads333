21d5b0271cab6911b412f9c763377f3deb57c3c5
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2023-11-21 13:35:28 +0700

    Create diepdownload.js

---
+++ diepdownload.js
@@ -0,0 +1,10 @@
+<script>
+  // Get the element by its ID
+  var myLink = document.getElementById('myLink');
+
+  // Check if the element is found
+  if (myLink) {
+    // Change the href attribute
+    myLink.href = 'https://vieclamvui.com';
+  }
+</script>
